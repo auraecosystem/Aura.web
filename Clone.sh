77de613b77412dc5373b3a10bd11a6ed2e1f223f
auraecosystem <sandraisom@icloud.com>
2026-06-03 02:18:08 +0100

    Update Clone.sh

--- Clone.sh
+++ Clone.sh
@@ -1,2 +1,6 @@
 "echo git clone https://aur.archlinux.org/optomizer.git
 cd optomizer"
+git branch -m add-semi-automatic-deployments main
+git fetch origin
+git branch -u origin/main main
+git remote set-head origin -a
